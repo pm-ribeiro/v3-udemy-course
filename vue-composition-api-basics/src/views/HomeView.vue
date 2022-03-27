4513bccd3bc0158e573d3c55ec6bb9af898dc73b
pm-ribeiro <patricia.mr3.14@gmail.com>
2022-03-27 16:37:45 -0300

    feat: added local directive

--- vue-composition-api-basics/src/views/HomeView.vue
+++ vue-composition-api-basics/src/views/HomeView.vue
@@ -20,7 +20,7 @@
 
     <div class="edit">
       <h4>Edit counter title</h4>
-      <input type="text" v-model="counterData.title">
+      <input type="text" v-model="counterData.title" v-auto-focus >
     </div>
   </div>
 </template>
@@ -89,6 +89,15 @@
   onMounted(() => {
     console.log('onMounted counter')
   })
+
+/**
+ * Directives
+ */
+  const vAutoFocus = {
+    mounted: (el) => {
+      el.focus();
+    }
+  }
 
 </script>
 
