9a59e714a0541baa8b0c6b3ff30e48d29e73c4b4
pm-ribeiro <patricia.mr3.14@gmail.com>
2022-03-27 09:59:25 -0300

    feat: added counter component with options api

--- vue-composition-api-basics/src/views/HomeView.vue
+++ vue-composition-api-basics/src/views/HomeView.vue
@@ -1,9 +1,42 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+<template>
+  <div class="home">
+    <button class="btn" @click="decrement">-</button>
+    <span class="counter">{{counter}}</span>
+    <button class="btn" @click="increment">+</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+
+  methods: {
+    increment() {
+      this.counter += 1;
+    },
+
+    decrement() {
+      this.counter -= 1;
+    }
+  }
+}
+
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+
+.btn, .counter {
+  font-size: 40px !important;
+  margin: 10px !important;
+}
+
+</style>
