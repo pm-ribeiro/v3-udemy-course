b1d03b322d43a0a74c97299650d6b18cc7c60fb2
pm-ribeiro <patricia.mr3.14@gmail.com>
2022-03-30 16:46:35 -0300

    feat: added new routes and views

---
+++ vue-composition-api-basics/src/views/PostsView.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="posts">
+    <h1>Posts</h1>
+
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        <RouterLink
+          :to="`/postDetail/${post.id}`"
+        >
+          Post {{post.id}}
+        </RouterLink>
+      </li>
+    </ul>
+
+    <textarea v-auto-focus/>
+  </div>
+</template>
+
+<script setup>
+/**
+ * Imports
+ */
+  import vAutoFocus from '@/directives/vAutoFocus';
+  import  {
+    reactive,
+  } from 'vue';
+
+  const posts = reactive(
+    [
+      { id: '1', title: 'Post 1' },
+      { id: '2', title: 'Post 2' },
+      { id: '3', title: 'Post 3' },
+    ]
+  )
+
+</script>
+
+<style scoped>
+ul {
+  margin-bottom: 30px;
+}
+</style>
