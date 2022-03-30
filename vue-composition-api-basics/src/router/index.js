49fe71c7654a236577d7ffea7f0dd47a06356974
pm-ribeiro <patricia.mr3.14@gmail.com>
2022-03-30 17:11:51 -0300

    feat: added modals

--- vue-composition-api-basics/src/router/index.js
+++ vue-composition-api-basics/src/router/index.js
@@ -24,6 +24,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/PostDetailView.vue')
+    },
+    {
+      path: '/modals',
+      name: 'modals',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ModalsView.vue')
     }
   ]
 })
---
+++ vue-composition-api-basics/src/views/ModalsView.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="modals">
+    <h1>Modals</h1>
+    <button  @click="showModal = true">Show modal</button>
+
+    <div class="modal-overlay" v-if="showModal">
+      <div class="modal">
+        <h2>This is a modal</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Assumenda debitis non accusantium facilis, placeat vel molestias totam, sit perferendis hic porro ad,
+          nostrum blanditiis dolore dolorum voluptatibus error ullam architecto.
+        </p>
+
+        <button @click="showModal = false">
+          hide modal
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+/**
+ * modals
+*/
+const showModal = ref(false);
+
+</script>
+
+<style scoped>
+.modal {
+  background-color: rgb(122, 122, 225);
+  padding: 12px;
+  margin: auto;
+  max-width: 50vh;
+  max-height: 80vh;
+}
+
+.modal-overlay {
+  background-color: rgb(43, 43, 43);
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  display: flex;
+  align-items: center;
+}
+</style>
