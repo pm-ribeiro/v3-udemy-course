1906ed2fd7aec2b767c756382021006063bdca31
pm-ribeiro <patricia.mr3.14@gmail.com>
2022-04-06 15:08:12 -0300

    feat: working with child compoents and added slots

---
+++ vue-composition-api-basics/src/components/Modal.vue
@@ -0,0 +1,56 @@
+<template>
+  <teleport to="body">
+    <div class="modal-overlay">
+      <div class="modal">
+
+      <slot name="title" />
+        <slot />
+
+        <pre>
+          {{ $slots.title}}
+        </pre>
+        <button @click="showModal = false">
+          hide modal
+        </button>
+      </div>
+    </div>
+  </teleport>
+
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import {useSlots} from 'vue';
+const slots = useSlots();
+
+console.log(slots.title())
+
+/**
+ * modals
+*/
+const showModal = ref(false);
+
+</script>
+
+<style scoped>
+.modal {
+  background-color: rgb(122, 122, 225);
+  padding: 12px;
+  margin: auto;
+  max-width: 50vh;
+  max-height: 80vh;
+  border-radius: 4px;
+}
+
+.modal-overlay {
+  background-color: rgb(43, 43, 43);
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  display: flex;
+  align-items: center;
+}
+</style>
--- vue-composition-api-basics/src/views/ModalsView.vue
+++ vue-composition-api-basics/src/views/ModalsView.vue
@@ -2,54 +2,25 @@
   <div class="modals">
     <h1>Modals</h1>
     <button  @click="showModal = true">Show modal</button>
-
-    <teleport to="body">
-      <div class="modal-overlay" v-if="showModal">
-        <div class="modal">
-          <h2>This is a modal</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Assumenda debitis non accusantium facilis, placeat vel molestias totam, sit perferendis hic porro ad,
-            nostrum blanditiis dolore dolorum voluptatibus error ullam architecto.
-          </p>
-
-          <button @click="showModal = false">
-            hide modal
-          </button>
-        </div>
-      </div>
-    </teleport>
+    <Modal v-if="showModal">
+      <template #title>
+        <h2>This is a modal title</h2>
+      </template>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Assumenda debitis non accusantium facilis, placeat vel molestias totam, sit perferendis hic porro ad,
+        nostrum blanditiis dolore dolorum voluptatibus error ullam architecto.
+      </p>
+    </Modal>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+import Modal from '@/components/Modal.vue';
 /**
  * modals
 */
 const showModal = ref(false);
 
 </script>
-
-<style scoped>
-.modal {
-  background-color: rgb(122, 122, 225);
-  padding: 12px;
-  margin: auto;
-  max-width: 50vh;
-  max-height: 80vh;
-  border-radius: 4px;
-}
-
-.modal-overlay {
-  background-color: rgb(43, 43, 43);
-  width: 100vw;
-  height: 100vh;
-  position: fixed;
-  top: 0;
-  left: 0;
-
-  display: flex;
-  align-items: center;
-}
-</style>
