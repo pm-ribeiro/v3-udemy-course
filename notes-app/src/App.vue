3c78c5d256de9bb95876d3676c3a4c15fc4d1c23
pm-ribeiro <patricia.mr3.14@gmail.com>
2022-04-16 17:22:19 -0300

    feat: added bulma

--- notes-app/src/App.vue
+++ notes-app/src/App.vue
@@ -1,6 +1,18 @@
 <template>
-  <RouterLink to="/">Notes</RouterLink> | 
+  <NavBar></NavBar>
+
+  <RouterLink to="/">Notes</RouterLink> |
   <RouterLink to="/stats">Stats</RouterLink>
 
   <RouterView />
 </template>
+
+<script setup>
+
+import NavBar from './components/Layout/NavBar.vue';
+
+</script>
+
+<style>
+  @import "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css";
+</style>
