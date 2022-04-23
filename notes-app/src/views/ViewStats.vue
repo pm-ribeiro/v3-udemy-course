42d108f5a4973dc279ba72753d49472d716cf5e3
pm-ribeiro <patricia.mr3.14@gmail.com>
2022-04-23 10:43:37 -0300

    feat: added status page and table component

--- notes-app/src/views/ViewStats.vue
+++ notes-app/src/views/ViewStats.vue
@@ -1,30 +1,14 @@
 <template>
   <div class="stats">
-    <h1>Stats</h1>
-    <table class="table is-fullwidth">
-      <thead>
-        <tr>
-          <th>Stat</th>
-          <th>Value</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Number of notes</td>
-          <td>{{storeNotes.totalNotesCount}}</td>
-        </tr>
-        <tr>
-          <td>Total of characters</td>
-          <td>{{storeNotes.totalCharacterestCount}}</td>
-        </tr>
-      </tbody>
-    </table>
+    <h1 class="title">
+      Stats
+    </h1>
+    <StatsTableVue />
   </div>
 </template>
 
 <script setup>
-import { useStoreNotes } from '@/stores/storeNotes';
 
-const storeNotes = useStoreNotes();
+import StatsTableVue from '@/components/StatsTable.vue';
 
 </script>
