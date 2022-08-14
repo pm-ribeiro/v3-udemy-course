d1e5d28b012752c8fc770a732a5f43f2d55fa4ee
pm-ribeiro <patricia.ribeiro@melhorenvio.com>
2022-08-14 16:38:34 -0300

    feat: added auth page and tabs

--- notes-app/src/router/index.js
+++ notes-app/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import ViewNotes from '@/views/ViewNotes.vue'
 import ViewStats from '@/views/ViewStats.vue'
 import ViewEditNote from '@/views/ViewEditNote.vue'
+import ViewAuth from '@/views/ViewAuth.vue'
 
 const routes = [
   {
@@ -18,7 +19,12 @@ const routes = [
     path: '/edit-note/:id',
     name: 'edit-note',
     component: ViewEditNote
-  }
+  },
+  {
+    path: '/auth/',
+    name: 'auth',
+    component: ViewAuth
+  },
 ]
 
 const router = createRouter({
---
+++ notes-app/src/views/ViewAuth.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="auth">
+    <div class="tabs is-centered">
+      <ul>
+        <li
+          :class="{ 'is-active': !register}"
+        >
+          <a @click="register = false">Login</a>
+        </li>
+        <li :class="{ 'is-active': register}">
+          <a @click="register = true">Register</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const register = ref(false);
+
+
+</script>
