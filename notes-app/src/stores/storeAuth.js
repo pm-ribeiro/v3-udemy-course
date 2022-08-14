54db65946e6ea4036cd97ad472f6a4479bed3aa2
pm-ribeiro <patricia.ribeiro@melhorenvio.com>
2022-08-14 17:30:45 -0300

    feat: added auth store

---
+++ notes-app/src/stores/storeAuth.js
@@ -0,0 +1,17 @@
+import { defineStore } from 'pinia'
+import { auth } from '@/js/firebase'
+
+export const useStoreAuth = defineStore('storeAuth', {
+  state: () => {
+    return {
+
+    }
+  },
+
+  getters: {
+
+  },
+
+  actions: {
+  }
+})
