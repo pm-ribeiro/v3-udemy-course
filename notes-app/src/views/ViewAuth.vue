03a0d6386908c51c2f2192b5b61be65a617a438b
pm-ribeiro <patricia.ribeiro@melhorenvio.com>
2022-08-14 16:41:35 -0300

    feat: added initial layout for login/register form

--- notes-app/src/views/ViewAuth.vue
+++ notes-app/src/views/ViewAuth.vue
@@ -12,13 +12,37 @@
         </li>
       </ul>
     </div>
+
+    <div class="card auth-form">
+      <div class="title has-text-centered">
+        {{formTitle}}
+      </div>
+      <div class="card-content">
+        <div class="content">
+          Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros.
+          Donec id elit non mi porta gravida at eget metus. Cum sociis natoque
+          penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cra
+          s mattis consectetur purus sit amet fermentum.
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 const register = ref(false);
 
+const formTitle = computed(() => {
+  return register.value ? 'Register' : 'Login';
+});
 
 </script>
+
+<style>
+.auth-form {
+  max-width: 400px;
+  margin: 0 auto;
+}
+</style>
